1a00ba910c06e293d6ee66cbee00712bf89ae508
nrhk <istnnrhk@gmail.com>
2018-08-10 14:33:09 +0900

    Add second servo (pin 13) and arm_controler program

---
+++ gpio_drill/Lab_arm_control/arm_control.py
@@ -0,0 +1,53 @@
+import wiringpi as pi, time
+
+pi.wiringPiSetupGpio()
+
+led_pin = 23
+servoA_pin = 18
+servoB_pin = 13
+
+# set pinMode
+pi.pinMode( led_pin, 1 )
+pi.pinMode( servoA_pin, 2 )
+pi.pinMode( servoB_pin, 2 )
+# set pwm
+pi.pwmSetMode(0)
+pi.pwmSetRange(1024)
+pi.pwmSetClock(375)
+
+# LED on
+pi.digitalWrite( led_pin, 1 )
+# Servo A : -90, Servo B : -90
+
+set_A_deg = -90
+move_A_deg = int( 74 + 48 / 90 + set_A_deg )
+pi.pwmWrite( servoA_pin, move_A_deg )
+
+set_B_deg = -90
+move_A_deg = int( 74 + 48 / 90 + set_B_deg )
+pi.pwmWrite( servoB_pin, move_B_deg )
+
+time.sleep(10)
+
+# Servo A : +90, Servo B : +90
+set_A_deg = 90
+move_A_deg = int( 74 + 48 / 90 + set_A_deg )
+pi.pwmWrite( servoA_pin, move_A_deg )
+
+set_B_deg = 90
+move_B_deg = int( 74 + 48 / 90 + set_B_deg )
+pi.pwmWrite( servoB_pin, move_B_deg )
+
+time.sleep(10)
+
+# Servo A :   0, Servo B :  0
+set_A_deg = 0
+move_A_deg = int( 74 + 48 / 90 + set_A_deg )
+pi.pwmWrite( servoA_pin, move_A_deg )
+
+set_B_deg = 0
+move_B_deg = int( 74 + 48 / 90 + set_B_deg )
+pi.pwmWrite( servoB_pin, move_B_deg )
+
+# LED off
+pi.digitalWrite( led_pin, 0 )
---
+++ gpio_drill/Lab_servo/servp_B_0.py
@@ -0,0 +1,14 @@
+import wiringpi as pi, time
+
+servo_pin = 13
+set_degree = 0
+
+pi.wiringPiSetupGpio()
+pi.pinMode( servo_pin, 2 )
+pi.pwmSetMode(0)
+pi.pwmSetRange(1024)
+pi.pwmSetClock(375)
+
+if ( set_degree <= 90 and set_degree >= -90 ):
+    move_deg = int( 74 + 48 / 90 * set_degree )
+    pi.pwmWrite( servo_pin, move_deg )
