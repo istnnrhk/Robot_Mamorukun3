14261e199926c25d5379d73dabbea8af566d23ed
nrhk <istnnrhk@gmail.com>
2018-08-11 18:47:13 +0900

    X1 has been completed to detect target mark and lasar pointer.

---
+++ X1/arm_control.py
@@ -0,0 +1,61 @@
+#-------------------------------------------------------
+#
+# arm-control program
+#
+#-------------------------------------------------------
+import sys
+import wiringpi as pi, time
+
+#-------------------------------------------------------
+# PARTS
+#-------------------------------------------------------
+def move( a_deg = 0 , b_deg = 0, waiting = 0):
+  a_deg_offset = 73
+  b_deg_offset = 80
+
+  move_A_deg = int( a_deg_offset - a_deg )
+  move_B_deg = int( b_deg_offset - b_deg )
+  pi.pwmWrite( servoA_pin, move_A_deg )
+  pi.pwmWrite( servoB_pin, move_B_deg )
+  time.sleep(waiting)
+  return
+
+#-------------------------------------------------------
+# Setup
+#-------------------------------------------------------
+pi.wiringPiSetupGpio()
+
+led_pin = 23
+servoA_pin = 18 # Altura
+servoB_pin = 13 # Azimuth
+
+# set pinMode
+pi.pinMode( led_pin, 1 )
+pi.pinMode( servoA_pin, 2 )
+pi.pinMode( servoB_pin, 2 )
+# set pwm
+pi.pwmSetMode(0)
+pi.pwmSetRange(1024)
+pi.pwmSetClock(375)
+
+#--------------------------------------------------------
+# MAIN PROCESS
+#--------------------------------------------------------
+pi.digitalWrite( led_pin, 1 ) # LED on
+#move(   0,   0, 0)
+
+#for y in range(-10, 10):
+#  for x in range(-5, 10):
+#    move( x, y, 0.1 )
+
+#move(  10,  15, 1 )
+#move(  10,  -5, 1 )
+#move( -10,  -5, 1 )
+#move( -10,  15, 1 )
+#move(   0,   0, 1 )
+
+args = sys.argv
+move( int(args[1]), int(args[2]), 0.1 )
+
+#pi.digitalWrite( led_pin, 0 ) # LED off
+
---
+++ X1/laser_detect.py
@@ -0,0 +1,20 @@
+import cv2
+
+im = cv2.imread('img.png')
+
+min_val, max_val, min_loc, max_loc = \
+        cv2.minMaxLoc(cv2.cvtColor(im,cv2.COLOR_RGB2GRAY))
+
+print("max loc = {}".format(max_loc))
+
+im[:, : ,(0, 1)] = 0
+
+min_val, max_val, min_loc, max_loc = \
+        cv2.minMaxLoc(cv2.cvtColor(im,cv2.COLOR_RGB2GRAY))
+
+print("max loc = {}".format(max_loc))
+
+cv2.imshow('led', im)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
